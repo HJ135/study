36dceeeeef3310d8109c6abb6e7198a94b68e897
문혁주 <@10.1.158.51>
2019-11-05 18:24:16 +0900

    BOJ_1001

---
+++ javaproject/src/package1/BOJ_1001.java
@@ -0,0 +1,17 @@
+package package1;
+
+import java.util.*;
+
+public class BOJ_1001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b;
+		Scanner scin = new Scanner(System.in);
+		
+		a = scin.nextInt();
+		b = scin.nextInt();
+		System.out.println(a-b);
+	}
+
+}
