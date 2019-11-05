4f89a551c5468e0fa7c04a6326bc0d5e1c208abb
문혁주 <@10.1.158.51>
2019-11-05 18:52:17 +0900

    python(not java)

--- javaproject/src/BOJ_DP/BOJ_1463_python.java
+++ javaproject/src/BOJ_DP/BOJ_1463_python.java
@@ -7,6 +7,6 @@ for i in range(1, number+1) :
     elif i % 3 == 0 : x = min(arr[i//3], arr[i-1])
     elif i % 2 == 0 : x = min(arr[i//2], arr[i-1])
     else : x = arr[i-1]
-    arr[i] = x+1
+    arr[i] = x+1 
 
 print(arr[number])
