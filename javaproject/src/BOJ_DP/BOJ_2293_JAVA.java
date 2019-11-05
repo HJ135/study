114880b7d3d9eab004238410e9fab596fee93af5
문혁주 <@10.1.158.51>
2019-11-05 18:31:39 +0900

    BOJ

---
+++ javaproject/src/BOJ_DP/BOJ_2293_JAVA.java
@@ -0,0 +1,43 @@
+package BOJ_DP;
+
+import java.util.*;
+
+public class BOJ_2293_JAVA {
+
+	public static void main(String[] args) {
+
+		int n, k; //배열의 size를 받는다.
+		Scanner scan = new Scanner(System.in);
+
+		n = scan.nextInt();
+		k = scan.nextInt();
+
+		int arr[][] = new int [n+1][k+1];
+
+		int ary[] = new int[n+1];
+		for(int i = 1 ; i <= n ; i ++) ary[i] = scan.nextInt();
+		Arrays.sort(ary);
+
+
+		for(int i = 1 ; i <= n ; i ++) {
+			arr[i][0] = ary[i];
+			for(int j = 1 ; j <= k ; j ++) {
+				if (i == 1 && j >= arr[i][0] && j % arr[i][0] == 0 ) arr[i][j] ++;
+				else if (j < arr[i][0]) arr[i][j] = arr[i-1][j]; 
+				else if (j == arr[i][0]) arr[i][j] = arr[i-1][j]+1;
+				else if (j > arr[i][0]) arr[i][j] = arr[i-1][j] + arr[i][j-arr[i][0]];
+			}
+		}
+
+		/*
+		for(int i = 0 ; i <= n ; i ++)
+			for(int j = 0 ; j <= k ; j ++) {
+				System.out.print(arr[i][j] + " ");
+				if (j == k) System.out.println();
+			}*/
+
+		System.out.println(arr[n][k]); // 정답
+
+		scan.close(); // scan 닫기
+	}
+}
